2d02b99276e5f7f4b646bcd91765ec5213ce6bf2
MartR <juneyourtech@users.noreply.github.com>
2016-12-30 05:58:06 +0200

    Update PM.user.js
    Caption work.

--- PM.user.js
+++ PM.user.js
@@ -3,7 +3,7 @@
 // @namespace     PMO_GM
 // @description	See kasutajaskript muudab PMO välimuse kasutajasõbralikumaks. Kaubamärgid kuuluvad nende vastavatele omanikele.
 // @updateURL https://github.com/juneyourtech/GM_PM/raw/master/PM.user.js
-// @version 0.8.6.0
+// @version 0.8.6.1
 // @include       *.postimees.ee/*
 // @include       http://www.60pluss.ee/*
 // @include       http://www.e24.ee/*
@@ -470,9 +470,22 @@ GM_addStyle("SPAN.hideInfoBar {float:left; margin-top:-5px; margin-left:0px; mar
 GM_addStyle("SPAN.gTitle {float:left; margin-right:16px;}") //
 
 //ENTIRE SCREEN / GALLERY LATE 2016 DESIGN
-GM_addStyle("DIV.fotorama__caption::before {background:transparent; display:none;}") //
+GM_addStyle("DIV.fotorama__caption {line-height:1.2em;}") //R., 30.12.2016.
 
-GM_addStyle("DIV.fotorama__caption__wrap > SPAN {background:linear-gradient(180deg, transparent 0px, rgba(0, 0, 0, 0.8) 70%) repeat scroll 0% 0%;}")
+GM_addStyle("DIV.fotorama__caption::before {background:transparent; display:none;}") //R., 30.12.2016.
+//This removes the big gradient from behind the image caption.
+
+GM_addStyle("DIV.fotorama .fotorama__caption__wrap {padding-left:1px; padding-bottom:0px;}") //
+
+//IMAGE COUNT
+GM_addStyle("SPAN[class*=fotorama__image__]:not([class=fotorama__image__count]) {background:linear-gradient(180deg, transparent 0px, rgba(0, 0, 0, 0.8) 70%) repeat scroll 0% 0%;}") //R., 30.12.2016.
+//DIV.fotorama__caption__wrap > SPAN
+
+//Image description font size
+GM_addStyle("DIV.fotorama .fotorama__caption__wrap .fotorama__image__caption {font-size:83%; line-height:unset; font-weight:500;}") //30.12.2016
+
+//avoid on hover
+GM_addStyle("DIV.fotorama .fotorama__caption__wrap .fotorama__image__caption:hover {visibility:hidden;}") //30.12.2016
 
 // kultuur.postimees.ee
 /*
