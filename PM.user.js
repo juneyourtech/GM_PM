ed3cc173c30b5651035085abe6a7345bfb927181
MartR <juneyourtech@users.noreply.github.com>
2017-01-08 06:06:44 +0200

    Update PM.user.js
    padding syntax fix

--- PM.user.js
+++ PM.user.js
@@ -3,7 +3,7 @@
 // @namespace     PMO_GM
 // @description	See kasutajaskript muudab PMO välimuse kasutajasõbralikumaks. Kaubamärgid kuuluvad nende vastavatele omanikele.
 // @updateURL https://github.com/juneyourtech/GM_PM/raw/master/PM.user.js
-// @version 0.8.6.8.5
+// @version 0.8.6.8.6
 // @include       *.postimees.ee/*
 // @include       http://www.60pluss.ee/*
 // @include       http://www.e24.ee/*
@@ -321,7 +321,7 @@ padding-top:0px; padding-left:3px; padding-right:3px; padding-bottom:1px; with f
 */
 
 //11.2016 redesign | [FRONTPAGE] top links to articles: specifics |
-GM_addStyle("DIV.article-content {margin:0px; margin-left:2px; margin-bottom:11px; pading-top:1px; padding-left:3px;}") //08.01.2017
+GM_addStyle("DIV.article-content {margin:0px; margin-left:2px; margin-bottom:11px; padding-top:1px; padding-left:3px;}") //08.01.2017
 
 //11.2016 redesign: background to specific words.
 GM_addStyle("A[href*=\"komisjon\"] > DIV.article-content, A[href*=\"komisjon\"]:visited > DIV.article-content {background-color:rgba(0, 0, 0, 0.7) !important;}") //08.01.2017
